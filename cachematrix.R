a00c395b25dedff2a469eef8af08706b9849e56d
hqhc <harishcherukuri@gmail.com>
2016-07-23 07:32:00 -0400

    Week 3, Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below ensure that the inverse of a matrix is computed
+## only once and if it is needed more than once, it is retrieved from a cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of four functions while also caching 
 
 makeCacheMatrix <- function(x = matrix()) {
+	## Initialize the Inverse to NULL
+    Inverse <- NULL
 
+	## Setter to assign variable y to x in the PARENT environment
+	## and NULL to the Inverse in the PARENT environment. Thus, if x is
+	## reset through a call to set function, the inverse is reset to NULL
+	## and cacheSolve function is forced to calculate the inverse of x.
+    set <- function(y) {
+        x <<- y
+        Inverse <<- NULL
+    }
+    get <- function() x
+    setInv <- function(Inv) Inverse <<- Inv
+    getInv <- function() Inverse
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return the inverse matrix of a square matrix.
+    ## No check is made to see if the input matrix is singular.
+    ## If the inverse is available in the cache, it is not calculated again.
+    Inverse <- x$getInv()
+    if(!is.null(Inverse)){
+        message("getting cached data")
+        return(Inverse)
+    }
+    M <- x$get()
+    Inverse <- solve(M) #solve(M) finds the inverse of M
+    x$setInv(Inverse) #cache the inverse
+    Inverse
 }
